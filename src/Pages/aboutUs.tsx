3b4fcc4a97454291d3bc92a2bbc1de46f350b5c2
mariyyah <ma_ria111@icloud.com>
2024-05-28 21:11:26 +0300

    modify the card

--- src/Pages/aboutUs.tsx
+++ src/Pages/aboutUs.tsx
@@ -3,10 +3,10 @@ import about from "../components/hero/PriImagPereview.png"
 export const AboutUs = () => {
   return (
     <>
-      <div className="about-Us lg:mb-10">
+      <div className=" about-Us lg:mb-10">
         <img
           src="https://images.pexels.com/photos/8942900/pexels-photo-8942900.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          className="about-image bg-[#BD9E82]"
+          className="sm:w-1/3 about-image bg-[#BD9E82]"
         />
         <div className="about-content">
           <p className="text-m font-bold text-gradient mb-3 md:text-l md:leading-[2rem] lg:text-xl lg:leading-[2rem] p-10">
@@ -17,7 +17,7 @@ export const AboutUs = () => {
         </div>
       </div>
       <div className="about-Us  flex-row-reverse">
-        <img src={about} className="about-image " />
+        <img src={about} className="sm:w-1/3 about-image " />
         <div className="about-content">
           <p className="text-m font-bold text-gradient md:text-l md:leading-[2rem] lg:text-xl lg:leading-[2rem] p-10">
             Welcome to Lion Chocolates, where bold flavors and exquisite craftsmanship meet. Indulge
