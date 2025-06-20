a731bed4a433c248339f45694d099e5f48b8edc9
Mariyyah Alrasheed <mariyyah.alrasheed@gmail.com>
2025-06-20 22:41:51 +0300

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,13 @@
 import axios from 'axios'
 
 const isDevelopment = import.meta.env.MODE === 'development'
-let baseURL = 'http://localhost:5125/api/v1'
+// let baseURL = 'http://localhost:5125/api/v1'
+let baseURL = 'https://e-commerce-website-backend-production-39d3.up.railway.app/api/v1'
 
-if (!isDevelopment) {
-  // Update this later when you have a working backend server
-  baseURL = 'https://e-commerce-website-backend-production-39d3.up.railway.app/api/v1'
-}
+// if (!isDevelopment) {
+//   // Update this later when you have a working backend server
+//   baseURL = 'https://e-commerce-website-backend-production-39d3.up.railway.app/api/v1'
+// }
 
 const api = axios.create({
   baseURL
